1fd01243fa88f3ad0cb80de32c7a1060134e7a13
dbetebenner <dbetebenner@gmail.com>
2017-03-21 14:40:25 -0400

    Tidying up data prep script

--- Guatemala_Data_LONG.R
+++ Guatemala_Data_LONG.R
@@ -10,24 +10,43 @@ require(data.table)
 require(SGP)
 
 
+### variables
+
+tmp.achievement_levels <- c("Insatisfactorio", "Necesita mejorar", "Satisfactorio", "Excelente")
+
 ### Load data
 
-Guatemala_Data_LONG <- fread("Data/Base_Files/COHORTE_1_LONG_FORMAT_SGP.csv", colClasses=rep("character", 15))
+Guatemala_Data_LONG <- fread("Data/Base_Files/ALLCOHORTS_LONG.csv", colClasses=rep("character", 7))
 
 
 ### Tidy up data
 
-Guatemala_Data_LONG[,LAST_NAME:=as.character(sapply(LAST_NAME, capwords))]
-Guatemala_Data_LONG[,FIRST_NAME:=as.character(sapply(FIRST_NAME, capwords))]
-Guatemala_Data_LONG[,SCHOOL_NAME:=as.character(sapply(SCHOOL_NAME, capwords))]
-Guatemala_Data_LONG[,DISTRICT_NAME:=as.character(sapply(DISTRICT_NAME, capwords))]
+setnames(Guatemala_Data_LONG, c("COD_ESTUDIANTE"), c("ID"))
+Guatemala_Data_LONG[,VALID_CASE:="VALID_CASE"]
+Guatemala_Data_LONG[,CONTENT_AREA:="READING"]
+Guatemala_Data_LONG[GRADE=="", GRADE:=NA]
+Guatemala_Data_LONG[ACHIEVEMENT_LEVEL=="", ACHIEVEMENT_LEVEL:=NA]
 Guatemala_Data_LONG[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
 
+Guatemala_Data_LONG[nchar(ID)==18, ID:=paste0("0", ID)]
+
+Guatemala_Data_LONG[,ACHIEVEMENT_LEVEL_ORIGINAL:=ACHIEVEMENT_LEVEL]
+Guatemala_Data_LONG[,ACHIEVEMENT_LEVEL:=NULL]
+Guatemala_Data_LONG <- SGP:::getAchievementLevel(Guatemala_Data_LONG, state="GUA")
+Guatemala_Data_LONG[GRADE %in% c("0", "4") & ACHIEVEMENT_LEVEL_ORIGINAL=="0", ACHIEVEMENT_LEVEL:=NA]
+Guatemala_Data_LONG[GRADE %in% c("0", "4") & ACHIEVEMENT_LEVEL_ORIGINAL=="1", ACHIEVEMENT_LEVEL:=tmp.achievement_levels[1]]
+Guatemala_Data_LONG[GRADE %in% c("0", "4") & ACHIEVEMENT_LEVEL_ORIGINAL=="2", ACHIEVEMENT_LEVEL:=tmp.achievement_levels[2]]
+Guatemala_Data_LONG[GRADE %in% c("0", "4") & ACHIEVEMENT_LEVEL_ORIGINAL=="3", ACHIEVEMENT_LEVEL:=tmp.achievement_levels[3]]
+Guatemala_Data_LONG[GRADE %in% c("0", "4") & ACHIEVEMENT_LEVEL_ORIGINAL=="4", ACHIEVEMENT_LEVEL:=tmp.achievement_levels[4]]
+Guatemala_Data_LONG[,ACHIEVEMENT_LEVEL_ORIGINAL:=NULL]
 
 ### Invalidate Cases
 
-Guatemala_Data_LONG[GRADE=="", VALID_CASE:="INVALID_CASE"]
 Guatemala_Data_LONG[is.na(SCALE_SCORE), VALID_CASE:="INVALID_CASE"]
+Guatemala_Data_LONG[is.na(GRADE), VALID_CASE:="INVALID_CASE"]
+setkey(Guatemala_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, ID, GRADE, SCALE_SCORE)
+setkey(Guatemala_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, ID)
+Guatemala_Data_LONG[which(duplicated(Guatemala_Data_LONG, by=key(Guatemala_Data_LONG)))-1, VALID_CASE:="INVALID_CASE"]
 
 
 ### Save Data
